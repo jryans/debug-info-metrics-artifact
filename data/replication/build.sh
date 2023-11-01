633854a5ff2f221df194b2cdadc5a184ec5ea17e
J. Ryan Stinnett <jryans@gmail.com>
2023-11-01 14:55:31 +0000

    Add scripts from ASPLOS replication

---
+++ data/replication/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -eux
+
+# Expects to run from replication repo directory
+if [ "${PWD##*/}" != "incomplete-debuginfo" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+source "${SCRIPT_DIR}/../vars.sh"
+
+# Source analysis
+echo "## Building testcases (source analysis)"
+
+## Build via `dbgcov` to report source code regions of various kinds
+export PATH="${DBGCOV_PATH}:${PATH}"
+DBGCOV_OPTS="$(dbgcov-cflags) -save-temps"
+for tc in $(find -H testcases -type d -depth 1 | cut -c 11- | sort -n); do
+  ${DBGCOV_CC} \
+    ${DBGCOV_OPTS} \
+    ${CC_SYSROOT_OPTS} \
+    -E \
+    -o testcases/${tc}/src/clang/a.i \
+    testcases/${tc}/src/clang/a.c
+done
---
+++ data/replication/check-quality.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -eux
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Expects to run from this script's directory
+if [ "${PWD}" != "${SCRIPT_DIR}" ]; then
+  echo "Does not appear to be the expected directory, abort!"
+  exit
+fi
+
+versions=(5 7 9 11)
+  levels=(2 3 g s)
+
+for tc in $(find -H testcases -type d -depth 1 | cut -c 11- | sort -n); do
+  for version in ${versions[*]}; do
+    for level in ${levels[*]}; do
+      echo "## Checking debug quality of testcases (${tc}, ${version}, ${level})"
+      debuginfo-quality \
+        --lines \
+        --tsv \
+        --only-locals \
+        --regions testcases/${tc}/src/clang/a.i.dbgcov \
+        --scope-regions \
+        --only-computation-regions \
+        --range-start-first-defined-region \
+        testcases/${tc}/bin/clang-${version}/opt-${level}.dSYM/Contents/Resources/DWARF/opt-${level} \
+        > \
+        testcases/${tc}/bin/clang-${version}/opt-${level}.tsv
+    done
+  done
+done
